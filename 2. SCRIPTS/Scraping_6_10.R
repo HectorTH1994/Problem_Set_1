2e74ff428db061fc6bd8e1e3e9c8c8aa55dbf1d4
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-04 13:07:15 -0500

    Se modifica el Scrip

--- 2. SCRIPTS/Scraping_6_10.R
+++ 2. SCRIPTS/Scraping_6_10.R
@@ -1,2 +1,8 @@
-2+2
-
+#Se llaman los paquetes necesarios para el Scraping
+library(tidyverse)
+library(pacman)
+library(rvest)
+library(data.table)
+library(datasets)
+#Se procede a llamar el chunk No. 6 de la base de datos GEIH: 
+Chunk6 <-read_html('https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_6.html')

3a3bec16847572b4b8e328aad99a08cfd70c6810
Hector Taticuan <hdtaticuanh@unal.edu.co>
2023-02-06 21:14:00 -0500

    Update Limpio.R

--- 2. SCRIPTS/Limpio.R
+++ 2. SCRIPTS/Limpio.R
@@ -3,10 +3,11 @@
 ### Carlos, Danna, Héctor, Alexa
 #Se prepara el espacio por medio del llamado a los paquetes y librerías: 
 library(pacman)
-p_load(tidyverse,rvest,writexl)
+
+p_load("tidyverse","rvest","writexl","stargazer","ggplot2","reshape2", "dplyr","datasets")
 library(data.table)
-library(datasets)
-library(dplyr)
+
+
 #Se realiza el scraping de los Chunks 6 a 10: 
 url <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_"
 data <- data.frame()
@@ -39,14 +40,78 @@ saveRDS(Base_datos_final, file = "Base_datos_final.rds")
 df<-(data %>%
           filter(age >= 18))
 
-##Con los datos que vamos a trabajar revisamos los datos faltantes para cada columna
+##Revisamos los datos faltantes para cada columna
 
 max(colSums(is.na(df)))
+colSums(is.na(df))
 
 #Dado que gran existe muchos datos faltantes, vemos cuantas y cuales columnas tiene mayor numero de faltantes
 length(which(colSums(is.na(df)) > 10000))
 
 ###Seleccionamos las columnas que son de nuestro interes
+##Importante Si no recibió salario en dinero, escriba 00; 
+#si recibió pero no sabe el monto, escriba 98; si no sabe si recibió, escriba 99
+#Horas extras p6510s1
 
-df[c("sex","age")]
 
+sum(is.na(df$y_salary_m_hu))
+nrow(df[!is.na(df$y_salary_m_hu),])
+
+
+sum(df$y_salary_m_hu > 0 & !is.na(df$y_salary_m_hu) )
+
+#Calculamos el porcentaje de los datos diferentes de NA
+sum(df$y_salary_m_hu > 0 & !is.na(df$y_salary_m_hu) )/length(df$y_salary_m_hu)
+
+
+
+df_anes <- na.omit(df[c("y_salary_m_hu","age")])
+df_anes$age_cuadrado <- df_anes$age^2
+
+
+str(df_anes)
+
+##Calculamos la matriz de correlaciones
+
+corr_matrix <- cor(df_anes)
+
+
+##Graficamos la matriz de correlaciones
+ggplot(melt(corr_matrix), aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 12, hjust = 1))
+
+##Histograma de las edades y de los salarios
+# dividmos el gráfico en dos paneles
+
+par(mfrow=c(1, 2))
+
+# histograma ingresos
+hist(df_anes$y_salary_m_hu)
+
+# histograma ingresos anes
+hist(df_anes$age)
+
+##Como se observa en el histograma de la izquiera existe un salario que nos altera la muestra
+##Grafica de dispersion
+
+ggplot(df_anes, aes(x = age, y = y_salary_m_hu)) +
+  geom_point(color = "red", size = 2) +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  theme_classic() +
+  labs(x = "Edad", y = "Salario por hora",
+       title = "Gráfico de dispersión edad vs salarios",
+       caption = "Datos de ejemplo")
+
+#Claramente se observa valores atipicos por lo cual se procede a eliminar los valores aticos
+
+
+par(mfrow = c(1, 2))
+boxplot(df_anes$y_salary_m_hu)
+boxplot(df_anes$age)
+
+
+#se procede a eliminar los valores de salarios que superen  la media +/- 1.5 veces la desviación 

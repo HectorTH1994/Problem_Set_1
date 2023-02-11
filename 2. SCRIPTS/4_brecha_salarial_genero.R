9ba7a1f470883dca93737566b2f425aef18991b7
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-11 15:32:26 -0500

    Se modifica el Script

--- 2. SCRIPTS/4_brecha_salarial_genero.R
+++ 2. SCRIPTS/4_brecha_salarial_genero.R
@@ -3,7 +3,7 @@
 ## Alexandran Rizo, Héctor Tacuán, Danna Bolaños, Carlos Vergara
 -------------------------------------------------------------------
 # 1- Inicio del proceso:
-#Se llaman las librerias y paquetes a ser utilizados: 
+##Se llaman las librerias y paquetes a ser utilizados: 
   
 library(pacman)
 
@@ -19,15 +19,28 @@ df_sin_atipicos <- import("df_sin_atipicos.rds")
 
 df <- import("df.rds")
 
-# 2- Se crean las variables logaritimas de ingreso "y_ingLab_m_ha" y sexo para las bases de dato utilizadas: 
+## Se crean las variables logaritimas de ingreso "y_ingLab_m_ha" y sexo para las bases de dato utilizadas: 
 
 df <-df%>% mutate(age2 = age*age)
-
 df <- df %>% mutate(logingtot=log(y_ingLab_m_ha))
-
 df_sin_atipicos <-df_sin_atipicos %>% mutate(age2 = age*age)
-
 df_sin_atipicos <- df_sin_atipicos %>% mutate(logingtot=log(y_ingLab_m_ha))
+df <- df %>% mutate(female = ifelse(sex == 0, 1, 0)) # se crea variable 1= mujer 0= hombre
+df_sin_atipicos <- df_sin_atipicos %>% mutate(female = ifelse(sex == 0, 1, 0))
+
+## Se genera un gráfico con las nuevas variables con fundamento en la información de ambas bases: 
+
+g_df<- ggplot(data=df) + 
+  geom_histogram(mapping = aes(x=logingtot , group=as.factor(female) , fill=as.factor(female)))
+
+histo_final_df <-g_df + scale_fill_manual(values = c("0"="orange" , "1"="red") , label = c("0"="Hombre" , "1"="Mujer") , name = "Sexo")
+
+g_df_sin_atipicos <- ggplot(data=df_sin_atipicos) + 
+  geom_histogram(mapping = aes(x=logingtot , group=as.factor(female) , fill=as.factor(female)))
+
+histo_final_df_sin_atipicos <- g_df_sin_atipicos + scale_fill_manual(values = c("0"="orange" , "1"="red") , label = c("0"="Hombre" , "1"="Mujer") , name = "Sexo")
+
+grid.arrange(histo_final_df, histo_final_df_sin_atipicos, ncol = 2)
 
 #El primer aspecto a realiozar es la estimación de la brecha salarial bajo la formula *
 ### Log(w)=B1+B2Female+u, donde w hace referencia a wage o salario. 

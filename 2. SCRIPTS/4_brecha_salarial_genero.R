dbbf4965e49a42bc4d71f45218b6685f82e0d6bf
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-11 15:02:22 -0500

    Se crean variables

--- 2. SCRIPTS/4_brecha_salarial_genero.R
+++ 2. SCRIPTS/4_brecha_salarial_genero.R
@@ -21,6 +21,13 @@ df <- import("df.rds")
 
 # 2- Se crean las variables logaritimas de ingreso "y_ingLab_m_ha" y sexo para las bases de dato utilizadas: 
 
+df <-df%>% mutate(age2 = age*age)
+
+df <- df %>% mutate(logingtot=log(y_ingLab_m_ha))
+
+df_sin_atipicos <-df_sin_atipicos %>% mutate(age2 = age*age)
+
+df_sin_atipicos <- df_sin_atipicos %>% mutate(logingtot=log(y_ingLab_m_ha))
 
 #El primer aspecto a realiozar es la estimación de la brecha salarial bajo la formula *
 ### Log(w)=B1+B2Female+u, donde w hace referencia a wage o salario. 
